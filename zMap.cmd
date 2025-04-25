0a13f5148d362a14f586c19512709a6d648243b3
batchhacker00 <batchhacker00@gmail.com>
2025-04-26 01:39:03 +0200

    Update zMap.cmd

--- zMap.cmd
+++ zMap.cmd
@@ -1,30 +1,35 @@
 @echo off
 setlocal enabledelayedexpansion
-title xTalon
+title zMap v2.0.1
 
-
-:main
-
-set succ=[34m[+][0m
-
-for /f "tokens=2 delims=:" %%d in ( ' ipconfig ^| findstr /i "IPv4" ' ) do set myip=%%d
-for /f "tokens=2 delims=:" %%a in ( ' ipconfig ^| findstr /i "IPv4" ' ) do set ip=%%a 
-rem take our local ip address
-
-set ip=%ip:~1%
-rem takes the first 3 number of our ip saving it on iproot variable
-for /f "tokens=1-3 delims=." %%a in ( "%ip%" ) do set iproot=%%a.%%b.%%c.
-
-echo you:%myip%
-echo ---------------------------------
-
-for /l %%i in ( 1,1,254 ) do (
-rem finds others ip address and dns/domain-name associated to it, by 1 to 254 
-for /f "tokens=2" %%b in ( 'ping -a -n 1 -w 10 %iproot%%%i ^| find "[" ' ) do (
-
-echo DNS:[%%b] IP:%iproot%%%i %succ% 
-
-)
+for /f "tokens=2 delims=:" %%a in ( 'ipconfig ^| findstr /i "IPv4"' ) do (
+    set "ip=%%a"
+    goto :gotIP
 )
 
-pause > nul && press nay key to close
+:gotIP
+set "ip=%ip:~1%"
+for /f "tokens=1-3 delims=." %%a in ("%ip%") do set "iproot=%%a.%%b.%%c."
+
+for /f "tokens=2" %%y in ( 'ping -a -n 1 -w 10 %ip% ^| find "[" ' ) do set mydns=%%y
+
+echo YOU: %ip% [%mydns%]
+echo ----------------------------
+
+
+for /l %%i in (1,1,254) do (
+    start /b cmd /c "ping -a -n 1 -w 10 %iproot%%%i | find "[" "
+)
+
+:waitloop
+tasklist | find /i "ping.exe" >nul
+if not errorlevel 1 (
+    timeout /t 1 >nul
+    goto :waitloop
+)
+echo ----------------------------
+echo.
+echo DONE
+echo.
+echo press any key to close
+pause > nul
